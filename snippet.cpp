1f2cae7cc61577a74c008ed90b0b2dcc84eee0a3
Shofiul <ytshofiul@gmail.com>
2021-12-14 18:51:03 +0600

    snippet.cpp added

---
+++ snippet.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+#define int long long int
+#define uint unsigned int
+#define pb push_back
+#define f first
+#define s second
+
+using namespace std;
+
+
+int32_t main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    return 0;
+}
+
+
